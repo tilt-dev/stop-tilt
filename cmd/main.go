4c6c6e65efa404c84e100251400eefc9dd033ff7
Maia <maia.mcc@gmail.com>
2021-05-12 17:24:15 -0400

    ask for user confirmation

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
+	"log"
 	"os"
+	"strings"
 
 	"github.com/tilt-dev/tilt-ci-status/pkg/stop"
 )
@@ -19,6 +22,10 @@ func run() error {
 
 	resourceNames := os.Args[1:]
 	if len(resourceNames) == 0 {
+		conf := askForConfirmation("Are you sure you want to kill your Tilt session? (Invoke this CLI with args to stop individual resources.)")
+		if !conf {
+			return nil
+		}
 		return s.StopTiltSession(ctx)
 	}
 
@@ -31,4 +38,24 @@ func main() {
 		_, _ = fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
 	}
+}
+
+func askForConfirmation(s string) bool {
+	// With thanks to https://gist.github.com/r0l1/3dcbb0c8f6cfe9c66ab8008f55f8f28b
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Printf("%s [Y/n]: ", s)
+
+	response, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	response = strings.ToLower(strings.TrimSpace(response))
+
+	if response == "n" || response == "no" {
+		return false
+	}
+
+	return true
 }
